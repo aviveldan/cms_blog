bcaaa76c5abf2d700a8f1d94997c6b0ac28aaf88
aviveldan <aviveldan@gmail.com>
2022-05-05 10:44:32 +0300

    coming soon

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,9 +19,11 @@ const Home: NextPage = () => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <Head>
-        <title>Aviv Eldan's website</title>
+        <title>Aviv Eldan</title>
         <link rel="icon" href="/ae_icon.png" />
       </Head>
+      <div className="flex justify-center">Coming soon</div>
+      {/*
       <div className="grid grid-cols-1 gap-12 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post: Post, index) => {
@@ -35,6 +37,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
+        */}
     </div>
   )
 }
